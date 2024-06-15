ab101dd7984e94a4bde7251f8e926890f94b0225
Zilzaer <josh.e.g.day@gmail.com>
2024-06-16 00:27:27 +0100

    package.json update
    made sure package.json included a "type": module,

--- Server/server.js
+++ Server/server.js
@@ -14,7 +14,7 @@ app.use(
 app.use(express.json()); //middleware to parse incoming json requests
 dotenv.config(); //loads environment variables from my .env file into process.env to keep my database connection string secure
 
-const dbConnectionstring = process.env.DATABASE_URL;
+const dbConnectionstring = process.env.DATABASE_URL; // retrieves the database connection string from the environment variables
 
 export const db = new PointerEvent.pool({
   connectionstring: dbConnectionstring,
@@ -26,18 +26,20 @@ db.connect((err, cleint, release) => {
   }
   console.log("Database connected successfully");
   release();
-});
+}); // i am using this log if the database successfully connects or if an error occured
 
 const port = 8080;
 app.listen(port, () => {
-  console.log(`Your server is running on port: ${port}`);
+  console.log(`Your server is running on port: ${port}`); //starts my server on my specified port and logs a message to indicate the server is running
 });
 
 app.get("/", (req, res) => {
-  res.json({ message: "There are no bad root route jokes, only bad taste." });
-});
+  res.json({
+    message: "There are no bad root route jokes, only bad taste. - Joe 2024",
+  });
+}); //used to define a GET route for the root URL ('/'). when the GET request is made to the root URL, the server resons with a JSON message
 
 app.use((err, req, res, next) => {
   console.error(err.stack);
   res.status(500).json({ error: "Something went wrong!" });
-}); //I am using this to handle errors when connecting to the database
+}); //error handling middleware to catch any errors that occur during the request-response cycle
