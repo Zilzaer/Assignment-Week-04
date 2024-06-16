00606d10943a12da427932d858fa5894b5e51ac0
Zilzaer <josh.e.g.day@gmail.com>
2024-06-16 02:24:28 +0100

    Update server.js
    readded dotenv.config

--- Server/server.js
+++ Server/server.js
@@ -3,6 +3,7 @@ import express from "express";
 import cors from "cors";
 import pg from "pg";
 import { createClient } from "@supabase/supabase-js";
+dotenv.config();
 
 // Create an instance of the express application
 const app = express();
